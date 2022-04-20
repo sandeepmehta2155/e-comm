cf8f0d616b863f3d089666fb1b7df63e0e262309
Sandeep Mehta <66156650+sandeepmehta2155@users.noreply.github.com>
2022-04-20 09:07:15 +0530

    Addded a h1 tag

--- src/App.js
+++ src/App.js
@@ -3,7 +3,8 @@ import "./styles.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
+      <h1>Hello CodeSandboxxxx</h1>
+      <h1>Helllo</h1>
       <h2>Start editing to see some magic happen!</h2>
     </div>
   );
